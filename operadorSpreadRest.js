3a517a2eea75a194d6d107181c670a6f626f54d5
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2020-08-28 09:07:15 -0300

    Melhoria na formataçao do codigo

--- operadorSpreadRest.js
+++ operadorSpreadRest.js
@@ -2,11 +2,11 @@
 // usar rest com parâmetro de função
 
 // usar spread com objeto
-const funcionario = { nome: 'Maria', salario: 12348.99 }
-const clone = { ativo: true, ...funcionario }
-console.log(clone)
+const funcionario = { nome: "Maria", salario: 12348.99 };
+const clone = { ativo: true, ...funcionario };
+console.log(clone);
 
 // usar spread com array
-const grupoA = ['João', 'Pedro', 'Gloria']
-const grupoFinal = ['Maria', ...grupoA, 'Rafaela']
-console.log(grupoFinal)
+const grupoA = ["João", "Pedro", "Gloria"];
+const grupoFinal = ["Maria", ...grupoA, "Rafaela"];
+console.log(grupoFinal);
